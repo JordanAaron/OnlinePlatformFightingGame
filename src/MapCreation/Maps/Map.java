fa3e6f48668ebe66b578f1c5caaf2b709108bf21
Jordan <jordev14@gmail.com>
2020-06-04 14:40:14 +0100

    Map creation setup
    Created an interface for maps that provides signatures to methods of necessary classes for each map. The testing map class implements the Map interface and Runnable as it will be a thread for matches played on this map.

---
+++ src/MapCreation/Maps/Map.java
@@ -0,0 +1,7 @@
+package MapCreation.Maps;
+
+import java.awt.*;
+
+public interface Map {
+    void draw(Graphics g);
+}
---
+++ src/MapCreation/Maps/TestingMap.java
@@ -0,0 +1,20 @@
+package MapCreation.Maps;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TestingMap extends JPanel implements Runnable, Map {
+    public TestingMap(){
+        setBackground(Color.black);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
